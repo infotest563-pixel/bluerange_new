fa3b8bfc3e4ee6993cbab934c959f4f9985fbceb
infotest563-pixel <infotest563@gmail.com>
2026-04-15 16:21:19 +0530

    ......

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['babel-jest', { configFile: './babel.config.js' }],
   },
   collectCoverageFrom: [
     'lib/**/*.{ts,tsx}',
